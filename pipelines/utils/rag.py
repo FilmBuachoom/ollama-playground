378751402cd4cc3d00ca64f8d23141a346486173
FilmBuachoom <filmbuachoom@gmail.com>
2024-08-09 15:11:23 +0700

    chore: format chat_text_qa_msgs

--- pipelines/utils/rag.py
+++ pipelines/utils/rag.py
@@ -5,7 +5,7 @@ from llama_index.core import ServiceContext, get_response_synthesizer, ChatPromp
 from llama_index.core.query_engine import RetrieverQueryEngine
 
 # Prompt function
-def create_prompt(user_message, vector_retriever):
+def create_prompt(user_message, docs_retriever):
     # Config prompt
     context_str = "Here is the context from the document(s)..."
 
@@ -13,32 +13,30 @@ def create_prompt(user_message, vector_retriever):
         "Context information is below.\n"
         "---------------------\n"
         "{context_str}\n"
-        "Relevant Document: {relevant_document}\n"
         "---------------------\n"
         "Given the context information and the relevant document, and not prior knowledge, "
         "answer the question: {query_str}\n"
     )
 
     # Render the initial QA prompt
-    qa_prompt_str = qa_prompt_str.format(context_str=context_str, relevant_document=vector_retriever, query_str=user_message)
+    qa_prompt_str = qa_prompt_str.format(context_str=docs_retriever, query_str=user_message)
     print(f"qa_prompt_str: {qa_prompt_str}")
 
-    refine_prompt_str = (
-        "We have the opportunity to refine the original answer "
-        "(only if needed) with some more context below.\n"
-        "------------\n"
-        "{context_msg}\n"
-        "Relevant Document: {relevant_document}\n"
-        "------------\n"
-        "Given the new context and the relevant document, refine the original answer to better "
-        "answer the question: {query_str}. "
-        "If the context isn't useful, output the original answer again.\n"
-        "Original Answer: {existing_answer}"
-    )
+    # refine_prompt_str = (
+    #     "We have the opportunity to refine the original answer "
+    #     "(only if needed) with some more context below.\n"
+    #     "------------\n"
+    #     "{context_msg}\n"
+    #     "------------\n"
+    #     "Given the new context and the relevant document, refine the original answer to better "
+    #     "answer the question: {query_str}. "
+    #     "If the context isn't useful, output the original answer again.\n"
+    #     "Original Answer: {existing_answer}"
+    # )
 
-    # Render the refine prompt
-    refine_prompt_str = refine_prompt_str.format(context_msg="Additional context provided...", relevant_document=vector_retriever, query_str=user_message, existing_answer="Original answer to refine")
-    print(f"refine_prompt_str: {refine_prompt_str}")
+    # # Render the refine prompt
+    # refine_prompt_str = refine_prompt_str.format(context_msg="Additional context provided...", relevant_document=vector_retriever, query_str=user_message, existing_answer="Original answer to refine")
+    # print(f"refine_prompt_str: {refine_prompt_str}")
 
     # Text QA Prompt
     chat_text_qa_msgs = [
@@ -51,17 +49,17 @@ def create_prompt(user_message, vector_retriever):
     text_qa_template = ChatPromptTemplate.from_messages(chat_text_qa_msgs)
     print(text_qa_template)
     
-    # Refine Prompt
-    chat_refine_msgs = [
-        (
-            "system",
-            "You are a tourism guide that provides accurate and helpful information based only on the following query documents. Always include references to the documents used in your response. คุณคือไกด์นำเที่ยวที่ให้ข้อมูลที่ถูกต้องและเป็นประโยชน์โดยอิงจากเอกสารคำถามเท่านั้น และคืนค่าการอ้างอิงถึงเอกสารที่ใช้ในคำตอบของคุณ",
-        ),
-        ("user", refine_prompt_str),
-    ]
-    refine_template = ChatPromptTemplate.from_messages(chat_refine_msgs)
-    print(refine_template)
-
+    # # Refine Prompt
+    # chat_refine_msgs = [
+    #     (
+    #         "system",
+    #         "You are a tourism guide that provides accurate and helpful information based only on the following query documents. Always include references to the documents used in your response. คุณคือไกด์นำเที่ยวที่ให้ข้อมูลที่ถูกต้องและเป็นประโยชน์โดยอิงจากเอกสารคำถามเท่านั้น และคืนค่าการอ้างอิงถึงเอกสารที่ใช้ในคำตอบของคุณ",
+    #     ),
+    #     ("user", refine_prompt_str),
+    # ]
+    # refine_template = ChatPromptTemplate.from_messages(chat_refine_msgs)
+    # print(refine_template)
+    refine_template = None
     return text_qa_template, refine_template
 
 # Retrieve function
@@ -79,14 +77,14 @@ def retrieve_func(index, top_k, user_message, service_context, text_qa_template)
         print('='*100)
 
     # Get prompt
-    text_qa_template, refine_template = create_prompt(user_message=user_message, vector_retriever=format_relevance_docs)
+    text_qa_template, refine_template = create_prompt(user_message=user_message, docs_retriever=format_relevance_docs)
 
     # Configure response synthesizer
     response_synthesizer = get_response_synthesizer(
         response_mode="refine",
         service_context=service_context,
         text_qa_template=text_qa_template,
-        refine_template=refine_template,
+        # refine_template=refine_template,
         use_async=False,
         streaming=False,
     )
