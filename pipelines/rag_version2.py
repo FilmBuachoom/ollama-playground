b9e23f850a52cad1d7cb86fa69b21b0e4c993ea1
FilmBuachoom <filmbuachoom@gmail.com>
2024-09-07 22:12:11 +0700

    feat: agent rag chat

---
+++ pipelines/rag_version2.py
@@ -0,0 +1,88 @@
+"""
+title: Llama Index Ollama Pipeline
+author: open-webui
+date: 2024-05-30
+version: 1.0
+license: MIT
+description: A pipeline for retrieving relevant information from a knowledge base using the Llama Index library with Ollama embeddings.
+requirements: llama-index, llama-index-llms-ollama, llama-index-embeddings-ollama
+"""
+
+from typing import List, Union, Generator, Iterator
+from schemas import OpenAIChatMessage
+import os
+
+from pydantic import BaseModel
+
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.text_embeddings_inference import TextEmbeddingsInference
+from llama_index.core import Settings
+from llama_index.core.agent import ReActAgent
+
+# import utility functions
+from pipelines.utils.agent_utils import VectorStoreManager, RewritingInput
+
+
+class Pipeline:
+
+    class Valves(BaseModel):
+        LLAMAINDEX_OLLAMA_BASE_URL: str
+        LLAMAINDEX_MODEL_NAME: str
+        LLAMAINDEX_EMBEDDING_MODEL_NAME: str
+
+    def __init__(self):
+        self.documents = None
+        self.index = None
+
+        self.valves = self.Valves(
+            **{
+                "LLAMAINDEX_OLLAMA_BASE_URL": os.getenv("LLAMAINDEX_OLLAMA_BASE_URL", "http://ollama:11434"),
+                "LLAMAINDEX_MODEL_NAME": os.getenv("LLAMAINDEX_MODEL_NAME", "llama3"),
+                "LLAMAINDEX_EMBEDDING_MODEL_NAME": os.getenv("LLAMAINDEX_EMBEDDING_MODEL_NAME", "nomic-embed-text"),
+                "TEXT_EMBEDDING_INFERENCE_BASE_URL": os.getenv("TEXT_EMBEDDING_INFERENCE_BASE_URL", "http://embedding-inference:11435"),
+            }
+        )
+
+    async def on_startup(self):
+        """This function is called when the server is started."""
+        # setup llm
+        Settings.embed_model = TextEmbeddingsInference(
+            base_url=self.valvesTEXT_EMBEDDING_INFERENCE_BASE_URL,
+            model_name="thenlper/gte-small",
+            auth_token=None,
+            timeout=120,
+            embed_batch_size=32,
+        )
+        Settings.llm = Ollama(
+            model=self.valves.LLAMAINDEX_MODEL_NAME,
+            base_url=self.valves.LLAMAINDEX_OLLAMA_BASE_URL,
+            temperature=0.4,
+            context_window=4096,
+            request_timeout=600
+        )
+
+        # defind global variables
+        global query_engine_tools, rewriting, agent
+
+        # load retriever tool
+        query_engine_tools = VectorStoreManager(path_to_folder="/app/pipelines/embedding").build_query_routing()
+
+        # load rewriting process
+        rewriting = RewritingInput()
+
+        # build agent
+        agent = ReActAgent.from_tools(query_engine_tools, llm=Settings.llm, verbose=True, max_iterations=20)
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        pass
+
+    def pipe(self, user_message: str, model_id: str, messages: List[dict], body: dict) -> Union[str, Generator, Iterator]:
+        """Typically, you would retrieve relevant information from your knowledge base and synthesize it to generate a response."""
+        # print(messages)
+        print(user_message)
+        query = rewriting.rewrite(query=user_message)
+        result = agent.chat(query)
+
+        return str(result.response)
---
+++ pipelines/utils/agent_utils.py
@@ -0,0 +1,131 @@
+# import
+import os, re
+from llama_index.core import Settings, load_index_from_storage, StorageContext, PromptTemplate
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+
+
+# Main class
+class VectorStoreManager:
+    def __init__(self, path_to_folder: str, top_k: int = 5):
+        """Initialize VectorStoreManager with a path and top_k value."""
+        self.path_to_folder = path_to_folder
+        self.top_k = top_k
+        self.llm = Settings.llm
+
+    def load_vector_store_idx(self):
+        """Load vector indices from different sources."""
+        # Load from source 1 (Lonely Planet)
+        storage_context_1 = StorageContext.from_defaults(persist_dir=os.path.join(self.path_to_folder, "multiple_chunk_size", "1024"))
+        vector_store_index_1 = load_index_from_storage(storage_context_1)
+        query_eng_1 = vector_store_index_1.as_query_engine(similarity_top_k=self.top_k, llm=self.llm)
+
+        # Load from source 2 (Amazing TH)
+        storage_context = StorageContext.from_defaults(persist_dir=os.path.join(self.path_to_folder, "vector_store_index_amazing_th"))
+        vector_store_index_2 = load_index_from_storage(storage_context)
+        query_eng_2 = vector_store_index_2.as_query_engine(similarity_top_k=self.top_k, llm=self.llm)
+
+        # Return both retrievers
+        return query_eng_1, query_eng_2
+
+    def build_query_routing(self, tools):
+        """Build query engine tool based on the loaded vector indices."""
+        # Define and return the QueryEngine
+        query_engine_lst = list()
+        for tool in tools.values():
+            query_engine_tool = QueryEngineTool(
+                query_engine=tool["tool"], metadata=ToolMetadata(name=tool["name"], description=tool["description"])
+            )
+            query_engine_lst.append(query_engine_tool)
+
+        return query_engine_lst
+
+    def load_query_engine_tool(self):
+        """Main function to load query engine for agent"""
+        # Load the vector store indices
+        query_eng_1, query_eng_2 = self.load_vector_store_idx()
+
+        # Adding description
+        tools = {
+            "tool_1": {
+                "name": "lonely_planet_vector_c1024",
+                "tool": query_eng_1,
+                "description": (
+                    "Useful for retrieving specific context about things that tourists need to know in Thailand."
+                    "Thai culture or etiquette. Include aspects like local customs, significant festivals, etiquette,"
+                    "traditional foods, basic language phrases, religious practices, and guidelines for respect at "
+                    "historical landmarks"
+                )
+            },
+            
+            "tool_2": {
+                "name": "amazing_th",
+                "tool": query_eng_2,
+                "description": "Useful for retrieving specific context about attractions in Thailand, include details such as"
+                "opening hours, admission fees, facilities, exact location, transportation options, type of attraction, "
+                "special events, dress code, and best visit times."
+            }
+        }
+        query_engine_tools = self.build_query_routing(tools=tools)
+
+        # return result
+        return query_engine_tools
+
+
+class RewritingInput:
+    def __init__(self):
+        """Rewriting and Expanding user query."""
+        self.llm = Settings.llm
+        self.prompt_template = {
+            'classify_prompt': (
+                "Given the user query, classify the main topic as either 'Other', 'Culture', or 'Attraction'. "
+                "Note that queries about suggestions, recommendations, or any topics that do not specifically relate "
+                "to social norms, traditions, etiquette, specific places, landmarks, or sightseeing points should be classified as 'Other'. "
+                "Queries about social norms, traditions, and etiquette fall under 'Culture.' "
+                "Queries about specific places, landmarks, or sightseeing points fall under 'Attraction'. "
+                "Return only 'Other', 'Culture', or 'Attraction' based on the user's input.\n\n"
+                "Here is the user query: {query}\n\n"
+                "Category:"
+
+            ),
+            
+            'rewriting_prompt': {
+                'Attraction': (
+                    "Generate a detailed question about attractions in Thailand in 64 words. Expand the query to include details such as "
+                    "opening hours, admission fees, facilities, exact location, transportation options, type of attraction, "
+                    "special events, dress code, and best visit times. Format your output in plain text.\n\n"
+                    "Here is the user query: {query}\n\n"
+                    "Question: "
+                ),
+                'Culture': (
+                    "Generate a detailed question about Thai culture or etiquette in 64 words. Include aspects like local customs, "
+                    "significant festivals, etiquette, traditional foods, basic language phrases, religious practices, and "
+                    "guidelines for respect at historical landmarks. Format your output in plain text.\n\n"
+                    "Here is the user query: {query}\n\n"
+                    "Question: "
+                )
+            }
+        }
+
+    async def rewrite(self, query: str):
+        "Entry point for rewriting, triggered by a StartEvent with `query`."
+        if query is None:
+            return None
+            
+        # classification the query with llm
+        classification_prompt_tmpl = PromptTemplate(self.prompt_template["classify_prompt"].format(query=query))
+        query_class = self.llm.predict(classification_prompt_tmpl).strip()
+        print(f">>> Input query:\n\t{query}\n\n")
+        print(f">>> User query about:\n\t{query_class}\n\n")
+        if len(re.findall(r'\b(recommend(?:s|ed|ation)?|suggest(?:s|ion)?)\b', input, re.IGNORECASE)) != 0 and query_class != "Other":
+            query_class = 'Other'
+            print(f">>> User query about:\n\t{query_class}\n\n")
+            
+       # Generate question based on topic
+        if query_class in ['Attraction', 'Culture']:
+            rewriting_prompt = PromptTemplate(self.prompt_template['rewriting_prompt'][query_class].format(query=query))
+            rewriting_query = self.llm.predict(rewriting_prompt).split("Question: ")[-1].strip()
+        else:
+            rewriting_query = query
+
+        # return
+        return rewriting_query
