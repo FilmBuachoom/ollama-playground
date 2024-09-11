6ec82474546d00fd5053fac53015989d66934456
FilmBuachoom <filmbuachoom@gmail.com>
2024-09-11 11:38:54 +0700

    fix: returning llm complete response

--- pipelines-RAG/agent_rag.py
+++ pipelines-RAG/agent_rag.py
@@ -77,6 +77,7 @@ class Pipeline:
         except:
             # exception when agent can't responce 
             result = Settings.llm.stream_complete(user_message)
+            print(result)
             return result
         
         # return result
