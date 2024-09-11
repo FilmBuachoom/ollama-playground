1f3244d0718ea183fd5975944100caf551f49cf7
FilmBuachoom <filmbuachoom@gmail.com>
2024-09-11 14:21:51 +0700

    fix: agent output

--- pipelines-RAG/agent_rag.py
+++ pipelines-RAG/agent_rag.py
@@ -82,4 +82,4 @@ class Pipeline:
         result = self.agent.chat(user_message)
         # print(f"\t{result.print_response_stream()}")
         # return result
-        return result
+        return result.response
